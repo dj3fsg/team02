04bc45ee61083ee257b75d89adb49afddf132578
t-y-9043 <227087803+t-y-9043@users.noreply.github.com>
2025-12-12 14:44:16 +0900

    DB設計1.00を実装し、マイグレーションとモデルの各ファイルを追加

---
+++ database/migrations/2025_12_12_115857_create_accounts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('accounts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('subcategory_id')->constrained();
+            $table->foreignId('type_id')->constrained();
+            $table->foreignId('status_id')->constrained();
+            $table->date('date');
+            $table->string('title', 255);
+            $table->decimal('amount', total: 8, places: 2);
+            $table->string('memo', 255);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('accounts');
+    }
+};
--- database/migrations/2025_12_12_115858_create_roles_table.php
+++ database/migrations/2025_12_12_115858_create_roles_table.php
@@ -11,14 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id(); //'id' BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY
+        Schema::create('roles', function (Blueprint $table) {
+            $table->id();
             $table->string('name', 100)->unique();
-            $table->string('email', 255)->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->foreignId('role_id')->constrained();
+            $table->string('desc', 255);
             $table->timestamps();
         });
     }
@@ -28,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('roles');
     }
 };
