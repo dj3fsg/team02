0ad2e64469be643cee497cb15fbbd751b5e93cda
honotaro <moca0106262@gmail.com>
2025-12-14 01:25:21 +0900

    G05: 当日詳細の基本表示と集計処理を実装

--- app/Http/Controllers/DayController.php
+++ app/Http/Controllers/DayController.php
@@ -4,13 +4,35 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Item;
+use App\Models\Account;
 
 class DayController extends Controller
 {
     public function show(string $date)
     {
-        return view('day.show',[
-            'date' => $date,
-        ]);
+       $items = Item::whereDate('sche_start', $date)
+            ->orderBy('sche_start')
+            ->get();
+
+       $accounts = Account::whereDate('date', $date)
+            ->orderBy('date')
+            ->get();
+
+        $incomeTotal = $accounts
+            ->where('amount', '>' , 0)
+            ->sum('amount');
+
+        $expenseTotal = $accounts
+        ->where('amount', '<', 0)
+        ->sum('amount');
+
+       return view('day.show', [
+        'date' => $date,
+        'items' => $items,
+        'accounts' => $accounts,
+        'incomeTotal' => $incomeTotal,
+        'expenseTotal' => $expenseTotal,
+       ]);
     }
 }
--- resources/views/day/show.blade.php
+++ resources/views/day/show.blade.php
@@ -9,5 +9,55 @@
 <body>
     <h1>G05 当日詳細一覧</h1>
     <p>日付：{{ $date }}</p>
+
+    <hr>
+
+    <h2>予定/タスク一覧</h2>
+
+    <table border="1" cellpadding="5">
+        <tr>
+            <th>時間</th>
+            <th>タイトル</th>
+            <th>状態</th>
+        </tr>
+
+    @forelse($items as $item)
+        <tr>
+            <td>{{ optional($item->sche_start)->format('H:i') }}
+                -
+                {{ optional($item->sche_end)->format('H:i') }}
+            </td>
+            <td>{{ $item->title }}</td>
+            <td>{{ $item->status_id }}</td>
+        </tr>
+    @empty
+        <tr>
+            <td colspan="3">予定はありません。</td>
+        </tr>
+    @endforelse
+    </table>>
+
+    <hr>
+
+    <h2>今日の収入/支出</h2>
+    
+    <table border="1" cellpadding="5">
+        <tr>金額</tr>
+        <tr>タイトル</tr>
+
+    @forelse($accounts as $account)
+        <tr>
+            <td>{{ $account->amount }}円</td>
+            <td>{{ $account->title }}</td>
+    @empty
+        <tr>
+            <td colspan="2">収支はありません</td>
+        </tr>
+    @endforelse
+
+<h3>今日の合計</h3>
+
+<p>収入合計：{{ number_format($incomeTotal) }}円</p>
+<p>支出合計：{{ number_format(abs($expenseTotal)) }}円</p>
 </body>
 </html>
